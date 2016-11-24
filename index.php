db3850aa3cf8b2cd3bdcacbd473afeefea46993f
Rob Lovelett <rlovelett@gmail.com>
2016-11-24 17:16:04 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <meta charset="UTF-8">
+    <title>Trivia</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <header>
+        <div>header text</div>
+    </header>    
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="twelve columns">
+                        <div id="timer"></div>
+                </div>
+            </div>
+        </div>    
+    </main>
+    <footer></footer>
+    <script type ="text/javascript" src="javascript/app.js"></script>
+  </body>
+</html>
---
+++ javascript/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+o_game = {
+    string = {
+        title_text: "Test Your Intelligence!",
+        start_text: "Start",
+        timer_text: "Time Remaining: ",
+        seconds_text: "Seconds",
+        game_over_text: "All Done!",
+        correct_text: "Correct Answers: ",
+        incorrect_text: "Incorrect Answers: ",
+        unanswered_text: "Unanswered: ",
+        question_text: "",
+        choice_text: "",
+
+    }
+}
+
+/*
+var thirty_seconds = 30, //init 30 seconds
+    display = $('#timer'), //target timer
+    
+
+f_start_timer(thirty_seconds, display); //call timer function
+
+function f_start_timer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(seconds);
+
+        if (--timer < 0 ) { //do this if timer reaches 0
+            timer = duration;
+        }
+    }, 1000);
+};
+*/
+
+
+}); //end document ready
