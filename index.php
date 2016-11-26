a38689eb0e5603e22fd1f8769abe8f0f3503700b
Rob Lovelett <rlovelett@gmail.com>
2016-11-26 00:23:14 -0600

    created game object, basic start screen, onclick go to game screen

--- index.php
+++ index.php
@@ -2,22 +2,12 @@
 <html lang="en-us">
   <head>
     <meta charset="UTF-8">
-    <title>Trivia</title>
+    <title>Trivia Game!</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   </head>
   <body>
-    <header>
-        <div>header text</div>
-    </header>    
-    <main>
-        <div class="container">
-            <div class="row">
-                <div class="twelve columns">
-                        <div id="timer"></div>
-                </div>
-            </div>
-        </div>    
-    </main>
+    <header></header>    
+    <main></main>
     <footer></footer>
     <script type ="text/javascript" src="javascript/app.js"></script>
   </body>
--- javascript/app.js
+++ javascript/app.js
@@ -1,20 +1,81 @@
 $(document).ready(function() {
 
-o_game = {
-    string = {
-        title_text: "Test Your Intelligence!",
-        start_text: "Start",
-        timer_text: "Time Remaining: ",
-        seconds_text: "Seconds",
-        game_over_text: "All Done!",
-        correct_text: "Correct Answers: ",
-        incorrect_text: "Incorrect Answers: ",
-        unanswered_text: "Unanswered: ",
-        question_text: "",
-        choice_text: "",
+    o_game = {
+        string: {
+            title_text: "Trivia Game!",
+            start_text: "Start",
+            timer_text: "Time Remaining: ",
+            seconds_text: "Seconds",
+            game_over_text: "All Done!",
+            correct_text: "Correct Answers: ",
+            incorrect_text: "Incorrect Answers: ",
+            unanswered_text: "Unanswered: ",
+            question_text: "", //init question text
+            choice_text: "", //init choice
+        flag: {
+            start_screen: true, //init start screen state flag
+            game_screen: false, //init game screen flag
+            game_over_screen: false, //init game over screen flag
+            correct_answer: false }, //init correct answer flag
+        counter: {
+            correct: 0, //init correct answer counter
+            incorrect: 0, //init incorrect answer counter
+            unanswered: 0, //init unanswered counter
+            questions: 0, }, //init # of questions 
+        int: {
+            max_questions: 8,
+            timer_max_seconds: 30, }
+    };
 
-    }
-}
+    f_start_screen(); //call start screen
+
+    function f_start_screen() {
+    
+        $(f_build_html("div", { id: "header_title" }, "<h1>" + o_game.string.title_text + "</h1>")).appendTo("header"); //create header div
+        $(f_build_html("button", { id: "start_game" }, o_game.string.start_text)).click(function() { //create start game button, if clicked, start
+            $(this).parent().remove(); //remove the button on click
+            o_game.flag.start_screen = false; //flag start screen to false
+            o_game.flag.game_screen = true; //flag game screen
+            f_game() //go to game screen on click
+        }).appendTo("main"); 
+
+    };
+
+    function f_game() {
+        //init & output questions/answers
+
+        //timer
+
+            //if timer reaches 0
+                //get all the user input/info
+                //go to game over screen
+
+        console.log("game screen");
+    };
+
+        // outputs: <a id="mylink" href="http://devalias.net">Glenn devalias Grant</a>
+        /*
+        f_build_html("div", "poop", {
+            id: "header"
+        });
+        
+        $("#header_title").html( //display o_start_textcreen.header_text
+            "<h1>" + o_game.string.header_text + "</h1>" +
+            "<h2>" + o_game.string.subtitle_text + "</h2>" 
+        );*/
+
+    function f_build_html(tag, attrs, inner_html) { //helper function to build html tags
+        var h = '<' + tag; //opening tag       
+        for (var attr in attrs) {
+            if(attrs[attr] === false) {
+                continue;
+            };
+            h += ' ' + attr + '="' + attrs[attr] + '"';
+        };
+        return h += inner_html ? '>' + inner_html + '</' + tag + '>' : '/>';
+    };
+    
+}); //end document ready
 
 /*
 var thirty_seconds = 30, //init 30 seconds
@@ -37,6 +98,3 @@ function f_start_timer(duration, display) {
     }, 1000);
 };
 */
-
-
-}); //end document ready
